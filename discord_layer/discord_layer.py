35377277f7c3c3f2b97d99cea0326d9a34a55c8b
Spencer Reyka <spencer.reyka@gmail.com>
2022-12-21 16:23:15 -0800

    added env loader class

--- discord_layer/discord_layer.py
+++ discord_layer/discord_layer.py
@@ -5,44 +5,7 @@ import os
 class DiscordProxy():
     def __init__(self):
         print("initializing discord proxy")
-        if "FISH_AUTH" in os.environ: 
-           self.auth = os.getenv('FISH_AUTH')
-        else:
-            print("No Fishing Auth in env")
-
-        self.retrieve_message_headers = {
-            'authorization': self.auth
-        }
-        
-        self.fish_headers = {
-            'Content-Type': 'application/json',
-            'authorization': self.auth
-        }
-
-        self.fish_payload = {
-            'type': 2,
-            'application_id': os.getenv('FISH_APPLICATION_ID'),
-            'channel_id': os.getenv('FISH_CHANNEL_ID'),
-            'guild_id': os.getenv('FISH_GUILD_ID'),
-            'session_id': os.getenv('FISH_SESSION_ID'),
-            'data': {
-                'version': os.getenv('FISH_VERSION'),
-                'id': os.getenv('FISH_ID'),
-                'name': "fish",
-                'type': '1',
-                #'options': [],# [parameters] if parameters else [],
-                #'application_command': '',# cmd_data,
-                #'attachments': []
-            },
-        }
-
-
+       
     def retrieve_messages(self):
-        r = requests.get(f'https://discord.com/api/v9/channels/1042344356520149004/messages?limit=5', headers=self.retrieve_message_headers)
-
-        return json.loads(r.text)
-
-    def fish(self):
-        print("fishing")
-
-        #r = requests.post(f'https://discord.com/api/v9/interactions', headers=self.fish_headers, json=self.fish_payload)
+        print("dummy retrivel")
+        return ""
--- runner.py
+++ runner.py
@@ -1,29 +1,10 @@
-import os
-import json
 from bot import start_loop
-
-
-def read_secrets() -> dict:
-    filename = os.path.join('config.json')
-    try:
-        with open(filename, mode='r') as f:
-            return json.loads(f.read())
-    except FileNotFoundError:
-        return {}
-
-def read_and_parse_secrets(): 
-    secrets = read_secrets()
-    for secret in secrets:
-        #print(secrets[secret])
-        if secret not in os.environ: 
-            os.environ[secret] = secrets[secret]
-            #print("adding" + secret + " to env")
-
+from env_reader import set_up_env
 
 def main():
     print("Starting runner!")
-    read_and_parse_secrets()
-    start_loop()
+    set_up_env()
+    #start_loop()
 
 if __name__ == "__main__":
     main()
